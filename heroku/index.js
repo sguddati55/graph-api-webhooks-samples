eac2c2815968d495961227f6a82005eee8d36b3c
sguddati55 <108351011+sguddati55@users.noreply.github.com>
2023-05-12 16:08:07 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -92,7 +92,7 @@ app.post('/webhook', function(req, res) {
       
       let reply = "sending test message";
       
-      if(msg_body.includes("optin")){
+      if(msg_body.includes("Welcome")){
         reply = "Yes";
       } else if(msg_body == "hi" || msg_body == "hello"){
         reply = "hi, how are you agent?";
